37a4aeaf978b5119fca3eb37b61b4e1f28bcd7c6
Luquitas234 <108678626+Luquitas234@users.noreply.github.com>
2025-07-18 19:27:01 -0300

    feat: Explicitly set Scalar UI route to /docs3

--- AcademiaNovit/Program.cs
+++ AcademiaNovit/Program.cs
@@ -29,11 +29,6 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connect
 
 builder.Services.AddOpenApi();
 
-builder.Services.AddScalarApiReference(options =>
-{
-    options.RoutePrefix = "docs"; // La UI de Scalar estará en /docs
-    options.SpecUrl = "/openapi.json"; // Asegura que apunte al JSON de OpenAPI
-});
 
 builder.Services.AddControllers();
 
